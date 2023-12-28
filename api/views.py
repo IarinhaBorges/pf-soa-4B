73e98402f41eefe19023756e75cb3be7a552fb57
IarinhaBorges <b.iara@escolar.ifrn.edu.br>
2023-12-28 09:38:50 -0300

    ajeitei a views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Post, Todo, Comment
+from .serializer import UserSerializer, PostSerializer, TodoSerializer, CommentSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de Artistas
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de Albuns
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de Músicas
+    """
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de Músicas
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
