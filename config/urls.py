3efe18e7ec671e09ce0c70bb7b0ba995c517285c
EuSolluna <sol.luna@escolar.ifrn.edu.br>
2023-12-28 09:43:10 -0300

    olha aí se nfoi

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,31 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from rest_framework_nested import routers
+from views import UserViewSet, PostViewSet
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from api import views
+
+router = routers.SimpleRouter()
+router.register(r'users', UserViewSet)
+
+router.register(r'users', views.UserViewSet)
+router.register(r'todos', views.UserViewSet)
+router.register(r'comments', views.UserViewSet)
+router.register(r'posts', views.UserViewSet)
+
+users_router = router.NestedSimpleRouter(router, r'users', lookup='user')
+users_router.register(r'users', views.TodoViewSet, basename='users-todos')
+
+todos_router = router.NestedSimpleRouter(router, r'users', lookup='user')
+todos_router.register(r'todos', views.TodoViewSet, basename='users-todos')
+
+# domains_router = routers.NestedSimpleRouter(router, r'domains', lookup='domain')
+# domains_router.register(r'nameservers', NameserverViewSet, basename='domain-nameservers')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include(router.urls)),
+    path(r'', include(users_router.urls)),
+   
 ]
