d6e437c883bb75e72cc50973de1d505c9504e354
IarinhaBorges <b.iara@escolar.ifrn.edu.br>
2023-12-28 09:54:47 -0300

    ok

---
+++ api/serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import User,Post, Todo, Comment
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
